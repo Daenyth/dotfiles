b3e8219d4812204b73cce59b4d7232548a63137e
Daenyth Blank <Daenyth+git@gmail.com>
2008-11-25 17:52:53 -0500

    trem.sh: stop scroll wheel from lagging the mouse pointer Signed-off-by: Daenyth Blank <Daenyth+git@gmail.com>

--- bin/trem.sh
+++ bin/trem.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NET_IP=$(ifconfig | fgrep -A 1 '00:0F:66:78:C1:4D' | tail -n 1 | awk '{print $2}' | sed 's/addr://')
+export SDL_VIDEO_X11_DGAMOUSE=0
 
 mpc pause
 xinit /home/Daenyth/games/tremulous/tremulous-mg.x86 +set net_ip $NET_IP $@ -- :1
