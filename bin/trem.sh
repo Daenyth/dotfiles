0b3886fcd81d9c5c61521069284abab9a8803279
Daenyth Blank <Daenyth+git@gmail.com>
2009-03-07 10:51:53 -0500

    bin/trem.sh: Change pidgin status when gaming

--- bin/trem.sh
+++ bin/trem.sh
@@ -3,5 +3,12 @@
 NET_IP=$(ifconfig | fgrep -A 1 '00:0F:66:78:C1:4D' | tail -n 1 | awk '{print $2}' | sed 's/addr://')
 export SDL_VIDEO_X11_DGAMOUSE=0
 
+AWAY_MSG='Playing Tremulous.
+Probably as )^(Daenyth'
+
 mpc pause
+status=$(purple-remote getstatus)
+message=$(purple-remote getstatusmessage)
+purple-remote "setstatus?status=away&message=$AWAY_MSG"
 xinit /home/Daenyth/games/tremulous/tremulous-mg.x86 +set net_ip $NET_IP $@ -- :1
+purple-remote "setstatus?status=$status&message=$message"
